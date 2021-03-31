fbe780a1d03ed13682f9eb4d8ce1611b2323c4ca
karantras <karantras@yandex.ru>
2021-03-31 17:17:47 +0300

    Add simple logging
    Добавлен виджет в GUI, который предоставляет отчет по выполненным операциям. На данный момент отчет ведется по Extractor.

--- GUI.py
+++ GUI.py
@@ -3,9 +3,10 @@
 
 import tkinter as tk
 import  tkinter.messagebox as mb
-from tkinter.ttk import Button, Style, Label
+from tkinter.ttk import Button, Style, Label, Scrollbar
 import utilities as ut
 import os
+import sys
 from configparser import ConfigParser
 
 configs = ConfigParser()
@@ -15,7 +16,6 @@ game_folder = configs.get('Folders', 'game folder')
 mods_folder = configs.get('Folders', 'mod folder')
 translator_folder = configs.get('Folders', 'translator folder')
 
-
 if translator_folder != os.path.dirname(os.path.abspath(__file__)):
 	trans_path = os.path.dirname(os.path.abspath(__file__))
 	configs['Folders']["translator folder"] = trans_path
@@ -24,31 +24,47 @@ if translator_folder != os.path.dirname(os.path.abspath(__file__)):
 	translator_folder = configs.get('Folders', 'translator folder')
 
 class Translator(tk.Frame):
-	
 	def __init__(self, parent):
-		tk.Frame.__init__(self, parent, background="#334")   
+		tk.Frame.__init__(self, parent, background="#333333")   
 		self.parent = parent
 		self.name = 'name'
 		self.parent.title("CDDA Translator")
-		self.text = tk.StringVar()
-		self.text.set("Current mod: "+ configs.get('Mods', 'mod'))
+		self.mod = tk.StringVar()
+		self.mod.set("Current mod: "+ configs.get('Mods', 'mod'))
+
+		modbar = tk.Frame(self, background="#424242")
+		modbar.pack(side="top", fill="x")
+		toolbar = tk.Frame(self, background="light grey")
+		toolbar.pack(side="top", fill="y")
+
+		self.text = tk.Text(self, wrap="word")
+		self.text.pack(side="top", fill="both", expand=True)
+		self.text.tag_configure("stderr", foreground="#b22222")
+		scroll = Scrollbar(command=self.text.yview)
+		scroll.pack(side="right", fill="y")
+		self.text.config(yscrollcommand=scroll.set)
+
+
 		self.pack(fill=tk.BOTH, expand=1)
 		self.centerWindow()
 
-		btn_file = Button(self, text="Select mod",command=self.select_mod)
-		btn_file.place(x=5, y=10)	
+		sys.stdout = TextRedirector(self.text, "stdout")
+		sys.stderr = TextRedirector(self.text, "stderr")
 
-		btn_replace = Button(self, text='Convert strings',command=self.converter)
-		btn_replace.place(x=173,y=10)
+		btn_file = Button(self, text="Select mod",command=self.select_mod, width = 14 )
+		btn_file.pack(in_=toolbar, side="left", padx = 3, pady = 1)	
 
-		btn_settings = Button(self, text='Extract strings',command=self.extractor)
-		btn_settings.place(x=85,y=10)
+		btn_convert = Button(self, text='Convert strings',command=self.converter, width = 14)
+		btn_convert.pack(in_=toolbar, side="left", padx = 3, pady = 1)	
 
-		dynlabel = Label(self, textvariable = self.text)
-		dynlabel.place (x = 0, y = 130)
+		btn_settings = Button(self, text='Extract strings',command=self.extractor, width = 14)
+		btn_settings.pack(in_=toolbar, side="left", padx = 3, pady = 1)	
+
+		dynlabel = Label(self, textvariable = self.mod, background="#424242", foreground ="#dddddd", font = "Courier 10 bold")
+		dynlabel.pack(in_= modbar, side = "bottom", fill ="y", anchor=tk.NW)
 
 	def centerWindow(self):
-		w = 267
+		w = 320
 		h = 150
 
 		sw = self.parent.winfo_screenwidth()
@@ -60,7 +76,7 @@ class Translator(tk.Frame):
 
 	def select_mod(self):
 		ut.select_mod(configs)
-		self.text.set("Current mod: " + configs.get('Mods', 'mod'))
+		self.mod.set("Current mod: " + configs.get('Mods', 'mod'))
 
 	def converter(self):
 		if configs.get('Mods', 'mod') == "NONE":
@@ -73,6 +89,17 @@ class Translator(tk.Frame):
 			print("Сначала выберите мод")
 		else:
 			ut.extractor(configs.get('Folders', 'mod folder'), configs.get('Mods', 'mod'))
+
+class TextRedirector(object):
+    def __init__(self, widget, tag="stdout"):
+        self.widget = widget
+        self.tag = tag
+
+    def write(self, str):
+        self.widget.configure(state="normal")
+        self.widget.insert("end", str, (self.tag,))
+        self.widget.configure(state="disabled")
+
 
 def main():
 	root = tk.Tk()
